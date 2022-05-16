3a32e6638a3b2c3aa75e068bd589a73a2a2eab5c
MrGawron <55848064+MrGawron@users.noreply.github.com>
2022-05-16 21:42:25 +0200

    one line upgraded

--- main.py
+++ main.py
@@ -3,8 +3,7 @@ import math
 def newton(n,k):
     return int(math.factorial(n)/(math.factorial(k)*math.factorial(n-k)))
 
-numbers = input()
-numbers = numbers.split()
+numbers = input().split()
 
 n = int(numbers[0])
 k = int(numbers[1])
